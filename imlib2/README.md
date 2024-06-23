e59ce15ae6c17b652b29046702f4fb0d2beb2493
demitas <demitas@tutanota.com>
2024-06-23 13:32:39 +0900

    image_hasher class

--- imlib2/README.md
+++ imlib2/README.md
@@ -2,6 +2,34 @@
 
 ```
 luarocks install --local lua-imlib2
+luarocks install --local md5
+luarocks install --local sha1
 ```
 
 [refer to deprecation error](https://stackoverflow.com/questions/37570975/lua-error-loading-limlib2-library-even-though-its-already-installed)
+
+
+```
+$ luarocks list
+Rocks installed for Lua 5.1
+---------------------------
+
+lua-cjson
+   2.1.0.10-1 (installed) - /home/demitas/.luarocks/lib/luarocks/rocks-5.1
+   2.1.0.10-1 (installed) - /usr/local/lib/luarocks/rocks-5.1
+
+lua-imlib2
+   0.1-4 (installed) - /home/demitas/.luarocks/lib/luarocks/rocks-5.1
+   0.1-4 (installed) - /usr/local/lib/luarocks/rocks-5.1
+
+luasocket
+   3.1.0-1 (installed) - /home/demitas/.luarocks/lib/luarocks/rocks-5.1
+
+md5
+   1.3-1 (installed) - /home/demitas/.luarocks/lib/luarocks/rocks-5.1
+   1.3-1 (installed) - /usr/local/lib/luarocks/rocks-5.1
+
+sha1
+   0.6.0-1 (installed) - /home/demitas/.luarocks/lib/luarocks/rocks-5.1
+   0.6.0-1 (installed) - /usr/local/lib/luarocks/rocks-5.1
+```
--- imlib2/generate_tilemap.lua
+++ imlib2/generate_tilemap.lua
@@ -3,39 +3,59 @@ local sha1 = require("sha1")
 local imlib2 = require("imlib2")
 
 
-function calculate_hash(image_path, hash_algorithm, cell_size_h, cell_size_v)
-    local hash_values = {}
+-- Define the class
+local ImageHasher = {}
+ImageHasher.__index = ImageHasher
 
-    local image_map = imlib2.image.load(image_path)
-    local width, height = image_map:get_width(), image_map:get_height()
-    print(string.format("image_map size (%d, %d)", width, height))
 
-    local n_cell_h = width / cell_size_h;
-    local n_cell_v = height / cell_size_v;
-    print(string.format(" cells (%d, %d)", n_cell_h, n_cell_v))
-    print(string.format(" total cells = %d", n_cell_h * n_cell_v))
-
-    for j = 0, n_cell_v - 1 do
-        for i = 0, n_cell_h - 1 do
-            local pixel_byte_array = get_cell_from_image(image_map, i, j, cell_size_h, cell_size_v)
-            local cell_hash = byte_array_to_hash(pixel_byte_array, hash_algorithm)
-            table.insert(
-                hash_values,
-                {
-                    cell_id = (j * n_cell_h + i),
-                    cell_hash = cell_hash,
-                    x = i,
-                    y = j,
-                }
-            )
-        end
-    end
-
-    return hash_values
+-- Constructor
+function ImageHasher:new()
+    local obj = setmetatable({}, self)
+    obj.hash_table = {}
+    return obj
 end
 
 
-function get_cell_from_image(image_map, cell_x, cell_y, cell_size_h, cell_size_v)
+-- Method to load png file from given path
+function ImageHasher:load_image(image_path, cell_size_h, cell_size_v, hash_algorithm)
+    local image = imlib2.image.load(image_path)
+    local width, height = image:get_width(), image:get_height()
+    local n_cell_h = width / cell_size_h;
+    local n_cell_v = height / cell_size_v;
+
+    for j = 0, n_cell_v - 1 do
+        for i = 0, n_cell_h - 1 do
+            local pixel_byte_array = self:_get_cell_from_image(image, i, j, cell_size_h, cell_size_v)
+            local cell_hash = self:_byte_array_to_hash(pixel_byte_array, hash_algorithm)
+            local cell_id = j * n_cell_h + i
+            self:_insert_hash(cell_id, cell_hash)
+        end
+    end
+end
+
+
+-- Public method to look up hash_table by cell_id
+function ImageHasher:lookup(cell_id)
+    for _, entry in ipairs(self.hash_table) do
+        if entry.cell_id == cell_id then
+            return entry.cell_hash
+        end
+    end
+    return nil
+end
+
+
+-- Private method to insert hash to hash_table
+function ImageHasher:_insert_hash(cell_id, cell_hash)
+    if cell_id == 0 then
+        print(cell_id, cell_hash)
+    end
+    table.insert(self.hash_table, {cell_id = cell_id, cell_hash = cell_hash})
+end
+
+
+-- Private method to extract byte array from cell rectangle area of the image
+function ImageHasher:_get_cell_from_image(image_map, cell_x, cell_y, cell_size_h, cell_size_v)
     local byte_array = ""
     for j = 0, cell_size_v - 1 do
         for i = 0, cell_size_h - 1 do
@@ -50,7 +70,8 @@ function get_cell_from_image(image_map, cell_x, cell_y, cell_size_h, cell_size_v
 end
 
 
-function byte_array_to_hash(byte_array, hash_algorithm)
+-- Private method to generate hash
+function ImageHasher:_byte_array_to_hash(byte_array, hash_algorithm)
     if hash_algorithm == 'md5' then
         return md5.sumhexa(byte_array)
     elseif hash_algorithm == 'sha1' then
@@ -59,24 +80,22 @@ function byte_array_to_hash(byte_array, hash_algorithm)
 end
 
 
+--
 -- main
 --
 local image_path_tilemap = "level1-working/Tilemap - Wall1.png"
 local image_path_tileset = "level1-working/tilemap-test1-Sheet-wall.png"
-local hash_algorithm = 'md5'  -- Change this to 'sha1' for SHA1 hash
 
-local cell_hashes = calculate_hash(image_path_tilemap, hash_algorithm, 16, 16)
--- Print the hash values of each cell
-for _, v in ipairs(cell_hashes) do
-    print(string.format(
-        " (%03d, %03d), %06d: %s",
-        v.x, v.y, v.cell_id, v.cell_hash))
-end
+local hasher = ImageHasher:new()
+local image_path = image_path_tilemap
+hasher:load_image(image_path, 16, 16, 'sha1')
 
-local cell_hashes = calculate_hash(image_path_tileset, hash_algorithm, 16, 16)
--- Print the hash values of each cell
-for _, v in ipairs(cell_hashes) do
-    print(string.format(
-        " (%03d, %03d), %06d: %s",
-        v.x, v.y, v.cell_id, v.cell_hash))
+
+-- Lookup hash for a specific cell_id
+local cell_id = 0
+local hash = hasher:lookup(cell_id)
+if hash then
+    print(string.format("Cell ID %d has hash %s", cell_id, hash))
+else
+    print(string.format("Cell ID %d not found", cell_id))
 end
